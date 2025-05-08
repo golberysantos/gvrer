cdf541cbe4836a29cddcb991cea2a3b413de89e6
golberysantos <golbery.santos@yahoo.com.br>
2025-05-07 21:38:43 -0400

    mer

--- src/main/java/br/com/gvrer/itinerario/repository/ItinerarioRepository.java
+++ src/main/java/br/com/gvrer/itinerario/repository/ItinerarioRepository.java
@@ -1,5 +1,32 @@
 package br.com.gvrer.itinerario.repository;
 
-public class ItinerarioRepository {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import org.springframework.stereotype.Service;
+
+import br.com.gvrer.config.ConexaoConfig;
+import br.com.gvrer.itinerario.model.Itinerario;
+
+@Service
+public class ItinerarioRepository {
+	public int criar(Itinerario itinerario) throws SQLException {
+
+		ConexaoConfig cnx = new ConexaoConfig();
+		Connection cnctn = cnx.conectar();
+		// cnctn.setAutoCommit(false);
+		Statement stmt = cnctn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+
+		String query = "" + "insert into itinerario"
+				+ "	(nomeitinerario, valor, observacoes, seguroviagem, dataida, horaida, horavolta, motivodaviagem_motivodaviagemid)"
+				+ "value" + "	('" + itinerario.getNomeitinerario() + "', " + itinerario.getValor()
+				+ "  , 'obs', 'seguro', '2025-05-01', '00:00:00', '00:00:00', 1)"
+
+		;
+
+		return stmt.executeUpdate(query);
+
+	}
 }
--- src/main/java/br/com/gvrer/itinerario/service/ItinerarioService.java
+++ src/main/java/br/com/gvrer/itinerario/service/ItinerarioService.java
@@ -1,7 +1,6 @@
 package br.com.gvrer.itinerario.service;
 
 import java.sql.Connection;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -9,28 +8,10 @@ import java.sql.Statement;
 import org.springframework.stereotype.Service;
 
 import br.com.gvrer.config.ConexaoConfig;
-import br.com.gvrer.itemrestaurante.model.Itemrestaurante;
 import br.com.gvrer.itinerario.model.Itinerario;
+
 @Service
 public class ItinerarioService {
-	
-	public int criar(Itinerario itinerario) throws SQLException {
 
-		ConexaoConfig cnx = new ConexaoConfig();
-		Connection cnctn = cnx.conectar();
-		// cnctn.setAutoCommit(false);
-		Statement stmt = cnctn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-		
-		
-		String query = ""
-				+"insert into itinerario"
-				+"	(nomeitinerario, valor, observacoes, seguroviagem, dataida, horaida, horavolta, motivodaviagem_motivodaviagemid)"
-				+"value"
-				+"	('"+itinerario.getNomeitinerario()+"', "+ itinerario.getValor() +"  , 'obs', 'seguro', '2025-05-01', '00:00:00', '00:00:00', 1)"
-				
-				;
 
-		return stmt.executeUpdate(query);
-
-		}
 }
